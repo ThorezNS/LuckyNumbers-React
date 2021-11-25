83e6f896d34fd658b42babacaa4369b80bc4acf4
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-11-25 14:32:01 +0100

    after drawing clean and next buttons appear

--- src/App.js
+++ src/App.js
@@ -16,18 +16,20 @@ function App() {
     const {numbers} = data;
     const {limitNumber, amountOfBalls} = status;
 
-    // const selectors = {
-    //     ballsContainer:'.balls',
-    //     ballsElements: '.ball',
-    //     numbersContainer: '.results',
-    //     generatorBtn: '.generator',
-    //     nextBtn: '.next',
-    //     clean: '.clean',
-    //     hide: 'hide',
-    //     disable: 'disabled'
-    // };
+    const selectors = {
+        ballsContainer:'.balls',
+        ballsElements: '.ball',
+        numbersContainer: '.results',
+        generatorBtn: 'generator',
+        nextBtn: 'generator next',
+        hide: 'hide',
+        disable: 'disabled'
+    };
 
     const [uiNumbers, setUInumbers] = useState([]);
+    const [generatorBtnClass, setGeneratorBtnClass] = useState(selectors.generatorBtn);
+    const [cleanBtnClass, setCleanBtnClass] = useState(selectors.hide);
+    const [nextBtnClass, setNextBtnClass] = useState(selectors.hide);
     //const [disable, setDisable] = useState(false);
 
     // const numbersContainer = document.querySelector(selectors.numbersContainer);
@@ -36,9 +38,6 @@ function App() {
     // const nextBtn = document.querySelector(selectors.nextBtn);
 
     useEffect(()=>{
-            // generateNumbers();
-            // cleanAll();
-            // displayNumbersList();
             fillTheArray();
     },[])
 
@@ -70,7 +69,7 @@ function App() {
         const number = getRandomNumber(limitNumber);
         putNumberToData(number);
         printBallNumber();
-        // buttons.display();
+        buttons.display();
     };
 
     // function displayNumbersList() {
@@ -117,7 +116,7 @@ function App() {
     //     buttons.disable();
     // };
 
-    // const generatorButton = {
+    const generatorButton = {
     //     disable() {
     //         setDisable(true);
     //         generatorBtn.classList.add(selectors.disable);
@@ -129,16 +128,16 @@ function App() {
     //         generatorBtn.removeAttribute('disabled', '');
     //     },
 
-    //     hide() {
-    //         generatorBtn.classList.add(selectors.hide);
-    //     },
+        hide() {
+            setGeneratorBtnClass(selectors.hide);
+        },
 
     //     show() {
     //         generatorBtn.classList.remove(selectors.hide);
     //     }
-    // };
+    };
 
-    // const buttons = {
+    const buttons = {
     //     disable() {
     //         nextBtn.classList.add(selectors.disable);
     //         nextBtn.setAttribute('disabled', '');
@@ -158,20 +157,20 @@ function App() {
     //         cleanBtn.classList.add(selectors.hide);
     //     },
 
-    //     show() {
-    //         nextBtn.classList.remove(selectors.hide);
-    //         cleanBtn.classList.remove(selectors.hide);
-    //     },
+        show() {
+            setNextBtnClass(selectors.nextBtn)
+            setCleanBtnClass(selectors.generatorBtn);
+        },
 
-    //     display() {
-    //         const lastBall = numbers.length === amountOfBalls;
-    //         if (lastBall) {
-    //             generatorButton.hide();
-    //             buttons.enable();
-    //             buttons.show();
-    //         };
-    //     }
-    // };
+        display() {
+            const lastBall = numbers.length === amountOfBalls;
+            if (lastBall) {
+                generatorButton.hide();
+                // buttons.enable();
+                buttons.show();
+            };
+        }
+    };
 
   return (
     <div className="container">
@@ -185,9 +184,9 @@ function App() {
         </div>
 
         <div className="drowning">
-            <button className="generator" onClick={ generateNumbers }>Generate numbers </button>
-            {/* <button className="generator clean hide" onClick={ cleanAll }>Clean all</button> */}
-            {/* <button className="generator next hide" onClick= { displayNumbersList }>Next</button> */}
+            <button className={ generatorBtnClass } onClick={ generateNumbers }>Generate numbers </button>
+            <button className={ cleanBtnClass }>Clean all</button>
+            <button className={ nextBtnClass }>Next</button>
         </div>
 
         <div className="results">
