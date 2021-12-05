149ea11d953bed9c318bdbdf12bcd92b903a22ab
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-12-05 20:18:46 +0100

    npm package used to generate a unique id for the kay property

--- src/components/Balls.js
+++ src/components/Balls.js
@@ -1,8 +1,10 @@
+import uniqid from 'uniqid';
+
 const Balls = ({numbers}) => {
     return (
         <div className="balls">
-            {numbers.map((number, index) => {
-               return <div key={index} className="ball">{number}</div>
+            {numbers.map((number) => {
+               return <div key={uniqid()} className="ball">{number}</div>
             })}
         </div>
      );
