e16a56ad8eaf8dd9a63a15f91631e84d98df3d1a
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-12-08 21:19:11 +0100

    decorating numbers with 0 in UI

--- src/components/Balls.js
+++ src/components/Balls.js
@@ -1,10 +1,17 @@
 import uniqueId from 'uniqid';
 
 const Balls = ({numbers}) => {
+    function addZeroToUnityNr(number) {
+        const unityNumber = number < 10;
+        if (unityNumber) {
+            return number = `0${number}`;
+        };
+        return number.toString();
+    };
     return (
         <div className="balls">
             {numbers.map((number) => {
-               return <div key={uniqueId()} className="ball">{number}</div>
+               return <div key={uniqueId()} className="ball">{addZeroToUnityNr(number)}</div>
             })}
         </div>
      );
