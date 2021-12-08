866dfcf435ed960748483e3def3c9dd26ccb09fd
Maciej Niemiec <niemiec.maciej@gmail.com>
2021-12-08 16:26:03 +0100

    button is greyed out during a draw

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import {useState} from 'react';
 import Balls from './components/Balls';
 
 function App() {
+  const selectors = {
+    generator: 'generator',
+    disabled: 'disabled'
+};
+
   const nrOfBalls = 6;
   const [numbers, setNumbers] = useState(fillWithEmptyBalls);
+  const [generatorBtnClass, setGeneratorBtnClass] = useState(selectors.generator);
+  const [disabled, setDisabled] = useState(false);
 
   function fillWithEmptyBalls() {
     const array = new Array(nrOfBalls);
@@ -12,7 +19,8 @@ function App() {
   }
 
   function generateNumbers() {
-    //generatorButton.disable();
+    setGeneratorBtnClass(`${selectors.disabled} ${selectors.generator}`);
+    setDisabled(true);
     for (let i = 0; i < nrOfBalls; i++) {
       setTimeout(() => {
         return setNumbers(displayBallNumber)
@@ -32,7 +40,8 @@ function App() {
         <h1 className="title">Lucky Lottery Numbers</h1>
         <Balls numbers={numbers}/>
         <div className="drowning">
-            <button className="generator"
+            <button className={generatorBtnClass}
+                    disabled={disabled}
                     onClick={generateNumbers}>Generate numbers</button>
         </div>
     </div>
