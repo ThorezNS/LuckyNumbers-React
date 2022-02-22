cd349ffe578698c2c5a982c392262ed566065e28
Maciej Niemiec <niemiec.maciej@gmail.com>
2022-02-22 22:38:22 +0100

    BarCounter component test

--- src/components/atoms/BarCounter/BarCounter.js
+++ src/components/atoms/BarCounter/BarCounter.js
@@ -1,12 +1,11 @@
 import './BarCounter.css';
 
-const BarCounter = ({drownNumbers, drownNrIndex}) => {
-
-    return (
-        <span className="drown-nr-wrapper__bar--counter">
-            {drownNumbers.length - drownNrIndex}:
-        </span>
-    )
-}
+const BarCounter = ({ drownNumbers, drownNrIndex }) => {
+  return (
+    <span data-testid="barCounter" className="drown-nr-wrapper__bar--counter">
+      {`${drownNumbers.length - drownNrIndex}:`}
+    </span>
+  );
+};
 
 export default BarCounter;
