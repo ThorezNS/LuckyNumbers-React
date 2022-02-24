d8d22731079c14ef03f2ea6eb9b067210e8b788b
Maciej Niemiec <niemiec.maciej@gmail.com>
2022-02-24 11:24:30 +0100

    NumberInTheBar component test

--- src/components/atoms/NumberInTheBar/NumberInTheBar.js
+++ src/components/atoms/NumberInTheBar/NumberInTheBar.js
@@ -2,13 +2,13 @@ import ChangeTypeAndDecorateNumber from '../../../containers/ChangeTypeAndDecora
 
 const NumberInTheBar = ({ arrayWithNr }) => {
   return (
-    <>
+    <div data-testid="numberInTheBar">
       {arrayWithNr.map((number, i) => {
         const isLastNr = arrayWithNr.length - 1 === i;
         const nr = ChangeTypeAndDecorateNumber(number);
         return isLastNr ? nr : `${nr}  .  `;
       })}
-    </>
+    </div>
   );
 };
 
